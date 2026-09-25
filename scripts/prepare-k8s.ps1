4f8689d1d1c21e06ae7df072aa0acfd15253c974
DEEP171819 <deepaksingh97813@gmail.com>
2026-09-25 11:14:33 +0530

    Make Kubernetes manifest rendering dynamic

--- scripts/prepare-k8s.ps1
+++ scripts/prepare-k8s.ps1
@@ -1,7 +1,10 @@
 $buildNumber = $env:BUILD_NUMBER
+$appId = $env:APP_ID
 
 $template = Get-Content "k8s\deployment.yaml" -Raw
 
-$rendered = $template -replace "IMAGE_TAG", $buildNumber
+$rendered = $template `
+    -replace "APP_ID", $appId `
+    -replace "IMAGE_TAG", $buildNumber
 
 Set-Content "k8s\deployment-rendered.yaml" $rendered
